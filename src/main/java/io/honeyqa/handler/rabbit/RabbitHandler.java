beeac446eb455929529f14ef233b4152495beed8
Minwoo Jung <jmwsoft@gmail.com>
2015-09-23 12:05:17 +0900

    reds update

--- src/main/java/io/honeyqa/handler/rabbit/RabbitHandler.java
+++ src/main/java/io/honeyqa/handler/rabbit/RabbitHandler.java
@@ -1,7 +1,52 @@
 package io.honeyqa.handler.rabbit;
 
-import java.net.SocketAddress;
+import java.io.IOException;
+
+import com.rabbitmq.client.*;
 
 public class RabbitHandler {
 
-}
+        private static final String TASK_QUEUE_NAME = "honey_queue";
+
+        public static void main(String[] argv) throws Exception {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            final Connection connection = factory.newConnection();
+            final Channel channel = connection.createChannel();
+
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+            System.out.println(" Waiting for messages");
+
+            channel.basicQos(1);
+
+            final Consumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    String message = new String(body, "UTF-8");
+
+                    System.out.println(" [x] Received '" + message + "'");
+                    try {
+                        doWork(message);
+                    } finally {
+                        System.out.println(" [x] Done");
+                        channel.basicAck(envelope.getDeliveryTag(), false);
+                    }
+                }
+            };
+            channel.basicConsume(TASK_QUEUE_NAME, false, consumer);
+        }
+
+        private static void doWork(String task) {
+            for (char ch : task.toCharArray()) {
+                if (ch == '.') {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException _ignored) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        }
+    }
+
+
